40e416fc291e9d0b96e50a02bdc8df5911adea77
Rin0530 <onochanbasket@gmail.com>
2020-10-27 22:48:10 +0900

    "animal"でランダムに画像を送信

--- hackason.py
+++ hackason.py
@@ -1,10 +1,17 @@
 import discord
 import TOKEN
 
+import random
+import subprocess
+
 TOKEN = TOKEN.TOKEN
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
+
+
+imageList = subprocess.check_output("ls /home/onochan/hackason/images/",shell=True).decode().replace("/", " ").split()
+numOfImages = len(imageList)-1
 
 # メッセージ受信時に動作する処理
 @client.event
@@ -12,6 +19,13 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+
+    elif message.content == "animal":
+        radomInt = random.randint(0,numOfImages)
+        await message.channel.send(file = discord.File(imageList[radomInt]))
+    #elif [client in message.mentions]:
+    #    subprocess.run("wget "+message.jump_url, shell= True)
+    #    subprocess.run("cp *.png *.jpeg images", shell=True)
 
 # 起動時に動作する処理
 @client.event
